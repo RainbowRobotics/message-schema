5009400e3905eab89f7f4211a52e8edfb4080a8e
dfd1123 <dfd1123@naver.com>
2025-09-13 02:01:07 +0900

    feat: 로봇 모델에 대한 상태 정보를 반환하는 새로운 API 엔드포인트 추가 및 상태 요청 스키마 정의

--- backend/services/manipulate/app/api/state/state_api_route.py
+++ backend/services/manipulate/app/api/state/state_api_route.py
@@ -1,17 +1,33 @@
 from app.modules.state.state_module_service import StateService
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from flat_buffers.IPC.State_Core import State_CoreT
+from rb_zenoh import zenoh_client
+from utils.parser import t_to_dict
 
 from .state_api_schema import (
     BaseControlResponsePD,
     PowerControlRequestPD,
     ReferenceControlRequestPD,
     ServoControlRequestPD,
+    StateRequestPD,
 )
 
 state_service = StateService()
 
 state_router = APIRouter()
+
+
+@state_router.get("/{robot_model}/state", response_model=StateRequestPD)
+async def state(robot_model: str):
+    topic, mv, obj, attachment = await zenoh_client.receive_one(
+        f"{robot_model}/state_core",
+    )
+
+    mv_bytes = bytes(mv)
+    obj = State_CoreT.InitFromPackedBuf(mv_bytes, 0)
+
+    return JSONResponse(t_to_dict(obj))
 
 
 @state_router.post("/{robot_model}/state/power", response_model=BaseControlResponsePD)
--- backend/services/manipulate/app/api/state/state_api_schema.py
+++ backend/services/manipulate/app/api/state/state_api_schema.py
@@ -16,3 +16,30 @@ class ServoControlRequestPD(BaseModel):
 
 class ReferenceControlRequestPD(BaseModel):
     reference_option: int
+
+
+class StateRequestPD(BaseModel):
+    joint_q_ref: list[float]
+    joint_q_enc: list[float]
+    joint_t_esti: list[float]
+    joint_t_meas: list[float]
+    joint_temper: list[float]
+
+    carte_x_ref: list[float]
+    carte_x_enc: list[float]
+
+    cbox_digital_input: list[int]
+    cbox_digital_output: list[int]
+
+    motion_mode: int
+    motion_speed_bar: float
+    motion_is_pause: int
+
+    status_lan2can: int
+    status_switch_emg: int
+    status_power_out: int
+    status_servo_num: int
+    status_is_refon: int
+    status_out_coll: int
+    status_self_coll: int
+    status_dt_mode: int
