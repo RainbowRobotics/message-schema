eee75bad4c1014def01a0ecc12019134ac0a57d8
dfd1123 <dfd1123@naver.com>
2025-09-18 22:02:00 +0900

    fix: flatbuffer 요청 처리 방식 개선 및 프로그램 서비스의 API 호출 방식 통일

--- backend/services/manipulate/app/modules/state/state_module_service.py
+++ backend/services/manipulate/app/modules/state/state_module_service.py
@@ -1,24 +1,15 @@
-import flatbuffers
 from app.modules.program.program_module_service import ProgramService
-from fastapi import HTTPException
 from flat_buffers.IPC.Request_PowerControl import (
-    Request_PowerControlAddPowerOption,
-    Request_PowerControlEnd,
-    Request_PowerControlStart,
+    Request_PowerControlT,
 )
 from flat_buffers.IPC.Request_ReferenceControl import (
-    Request_ReferenceControlAddRefcontrolOption,
-    Request_ReferenceControlEnd,
-    Request_ReferenceControlStart,
+    Request_ReferenceControlT,
 )
 from flat_buffers.IPC.Request_ServoControl import (
-    Request_ServoControlAddServoOption,
-    Request_ServoControlEnd,
-    Request_ServoControlStart,
+    Request_ServoControlT,
 )
-from flat_buffers.IPC.Response_Functions import Response_Functions
+from flat_buffers.IPC.Response_Functions import Response_FunctionsT
 from rb_zenoh import zenoh_client
-from rb_zenoh.exeption import ZenohNoReply, ZenohReplyError, ZenohTransportError
 
 program_service = ProgramService()
 
@@ -28,132 +19,83 @@ class StateService:
         pass
 
     async def power_control(
-        self, *, robot_model: str, power_option: int, sync_servo: bool, stoptime: int | None = 0.5
+        self, *, robot_model: str, power_option: int, sync_servo: bool, stoptime: int | None = 3
     ):
-        # ObjectAPI
-        # try:
-        #     req = Request_PowerControlT()
-        #     req.powerOption = 1
-
-        #     b = flatbuffers.Builder(128)
-        #     b.Finish(req.Pack(b))
-        #     fb_bytes = bytes(b.Output())
-
-        #     res = zenoh_client.query_one("example/function_powercontrol", payload=fb_bytes)
-
-        #     buf = res["payload"]
-        #     res = Response_PowerControlT.InitFromPackedBuf(buf, 0)
-
-        # except Exception as e:
-        #     raise HTTPException(status_code=502, detail=str(f"flatbuffers decode error: {e}")) from e
-
-        # return res
-
-        # TableAPI
-        try:
-            b = flatbuffers.Builder(32)
-            Request_PowerControlStart(b)
-            Request_PowerControlAddPowerOption(b, power_option)
-            root = Request_PowerControlEnd(b)
-            b.Finish(root)
-            power_fb_payload = bytes(b.Output())
-
-            power_res = zenoh_client.query_one(
-                f"{robot_model}/call_powercontrol", payload=power_fb_payload
+        if power_option == 0:
+            dict_reference_res = await self.reference_control(
+                robot_model=robot_model, reference_option=0
             )
 
-            tbl = Response_Functions.GetRootAs(power_res["payload"], 0)
-            power_return_value = tbl.ReturnValue()
+            reference_return_value = dict_reference_res.get(
+                "returnValue", dict_reference_res.get("return_value", None)
+            )
 
-            if power_return_value != 0:
-                return {
-                    "error": True,
-                    "error_target": "call_powercontrol",
-                    "return_value": power_return_value,
-                }
+            if reference_return_value != 0:
+                dict_reference_res["target"] = "call_referencecontrol"
+                return dict_reference_res
 
-            return_value = power_return_value
-
-            if power_option == 0:
-                self.reference_control(robot_model=robot_model, reference_option=0)
-
-                if stoptime is not None:
-                    program_service.call_smoothjog_stop(robot_model=robot_model, stoptime=stoptime)
-
-            elif power_option == 1 and sync_servo:
-                servo_return_value = await self.servo_control(
-                    robot_model=robot_model, servo_option=1
+            if stoptime is not None:
+                dict_smoothjog_stop_res = await program_service.call_smoothjog_stop(
+                    robot_model=robot_model, stoptime=stoptime
                 )
 
-                return_value = servo_return_value["return_value"]
+                smoothjog_stop_return_value = dict_smoothjog_stop_res.get(
+                    "returnValue", dict_smoothjog_stop_res.get("return_value", None)
+                )
 
-        except (ZenohNoReply, ZenohReplyError, ZenohTransportError):
-            raise
-        except Exception as e:
-            raise HTTPException(status_code=502, detail=str(f"error: {e}")) from e
+                if smoothjog_stop_return_value != 0:
+                    dict_smoothjog_stop_res["target"] = "call_smoothjog_stop"
+                    return dict_smoothjog_stop_res
 
-        return {"return_value": return_value}
+        elif power_option == 1 and sync_servo:
+            dict_servo_res = await self.servo_control(robot_model=robot_model, servo_option=1)
+
+            servo_return_value = dict_servo_res.get(
+                "returnValue", dict_servo_res.get("return_value", None)
+            )
+
+            if servo_return_value != 0:
+                dict_servo_res["target"] = "call_servocontrol"
+                return dict_servo_res
+
+        req = Request_PowerControlT()
+        req.powerOption = power_option
+
+        power_res = zenoh_client.query_one(
+            f"{robot_model}/call_powercontrol",
+            flatbuffer_req_obj=req,
+            flatbuffer_res_T_class=Response_FunctionsT,
+            flatbuffer_buf_size=32,
+        )
+
+        dict_power_res = power_res["dict_payload"]
+
+        dict_power_res["target"] = "call_powercontrol"
+
+        return dict_power_res
 
     async def servo_control(self, *, robot_model: str, servo_option: int):
-        try:
-            b = flatbuffers.Builder(32)
-            Request_ServoControlStart(b)
-            Request_ServoControlAddServoOption(b, servo_option)
-            root = Request_ServoControlEnd(b)
-            b.Finish(root)
-            servo_fb_payload = bytes(b.Output())
+        req = Request_ServoControlT()
+        req.servoOption = servo_option
 
-            servo_res = zenoh_client.query_one(
-                f"{robot_model}/call_servocontrol", payload=servo_fb_payload
-            )
+        servo_res = zenoh_client.query_one(
+            f"{robot_model}/call_servocontrol",
+            flatbuffer_req_obj=req,
+            flatbuffer_res_T_class=Response_FunctionsT,
+            flatbuffer_buf_size=32,
+        )
 
-            tbl = Response_Functions.GetRootAs(servo_res["payload"], 0)
-            return_value = tbl.ReturnValue()
-
-            if return_value != 0:
-                return {
-                    "error": True,
-                    "error_target": "call_servocontrol",
-                    "return_value": return_value,
-                }
-
-        except (ZenohNoReply, ZenohReplyError, ZenohTransportError):
-            raise
-        except Exception as e:
-            raise HTTPException(
-                status_code=502, detail=str(f"flatbuffers decode error: {e}")
-            ) from e
-
-        return {"return_value": return_value}
+        return servo_res["dict_payload"]
 
     async def reference_control(self, *, robot_model: str, reference_option: int):
-        try:
-            b = flatbuffers.Builder(32)
-            Request_ReferenceControlStart(b)
-            Request_ReferenceControlAddRefcontrolOption(b, reference_option)
-            root = Request_ReferenceControlEnd(b)
-            b.Finish(root)
-            reference_fb_payload = bytes(b.Output())
+        req = Request_ReferenceControlT()
+        req.refcontrolOption = reference_option
 
-            reference_res = zenoh_client.query_one(
-                f"{robot_model}/call_referencecontrol", payload=reference_fb_payload
-            )
+        reference_res = zenoh_client.query_one(
+            f"{robot_model}/call_referencecontrol",
+            flatbuffer_req_obj=req,
+            flatbuffer_res_T_class=Response_FunctionsT,
+            flatbuffer_buf_size=32,
+        )
 
-            tbl = Response_Functions.GetRootAs(reference_res["payload"], 0)
-            return_value = tbl.ReturnValue()
-
-            if return_value != 0:
-                return {
-                    "error": True,
-                    "error_target": "call_referencecontrol",
-                    "return_value": return_value,
-                }
-
-        except (ZenohNoReply, ZenohReplyError, ZenohTransportError):
-            raise
-        except Exception as e:
-            raise HTTPException(
-                status_code=502, detail=str(f"flatbuffers decode error: {e}")
-            ) from e
-
-        return {"return_value": return_value}
+        return reference_res["dict_payload"]
