948a75312ae54cf2d52c49012d7a44ae8ccd122d
dfd1123 <dfd1123@naver.com>
2025-09-22 20:13:47 +0900

    fix: ZenohRouter의 구독 패턴을 수정하여 모든 상태 코어 메시지를 수신하도록 변경

--- backend/services/manipulate/app/zenoh_subs/state.py
+++ backend/services/manipulate/app/zenoh_subs/state.py
@@ -7,10 +7,9 @@ from app.socket import socket_client
 zenoh_state_router = ZenohRouter()
 
 
-@zenoh_state_router.subscribe("cobot/state_core", flatbuffer_obj_t=State_CoreT)
+@zenoh_state_router.subscribe("*/state_core", flatbuffer_obj_t=State_CoreT)
 async def on_zenoh_sub_state(*, topic, mv, obj, attachment):
     try:
-
         await socket_client.emit(topic, obj)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(f"error: {e}")) from e
