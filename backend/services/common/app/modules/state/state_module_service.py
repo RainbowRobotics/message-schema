b56c90a48a4c9299cb6770c07b6316fec9aac431
dfd1123 <dfd1123@naver.com>
2025-09-22 20:15:12 +0900

    feat: 시스템 상태 조회 및 반복 호출 기능 추가

--- backend/services/common/app/modules/state/state_module_service.py
+++ backend/services/common/app/modules/state/state_module_service.py
@@ -1,11 +1,20 @@
+import asyncio
+import socket
+import time
+
 import flatbuffers
+import psutil
 from app.modules.program.program_module_service import ProgramService
+from app.socket import socket_client
 from fastapi import HTTPException
+from flat_buffers.IPC.Request_CallWhoAmI import Request_CallWhoAmIT
 from flat_buffers.IPC.Request_PowerControl import (
     Request_PowerControlT,
 )
 from flat_buffers.IPC.Request_ServoControl import Request_ServoControlT
+from flat_buffers.IPC.Response_CallWhoamI import Response_CallWhoamIT
 from flat_buffers.IPC.Response_Functions import Response_FunctionsT
+from flat_buffers.IPC.State_Core import State_CoreT
 from rb_zenoh import zenoh_client
 from rb_zenoh.exeption import ZenohNoReply, ZenohReplyError, ZenohTransportError
 from utils.parser import t_to_dict
@@ -15,28 +24,83 @@ programService = ProgramService()
 
 class StateService:
     def __init__(self):
-        pass
+        self._all_connect_state_period = 1
+        self._prev_all_connected_status = None
 
-    async def get_all_robots_info(self):
-        try:
-            results = await zenoh_client.query("whoami", timeout=2000)
+    async def get_system_state(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        res = self.get_all_robots_info()
 
-            services = []
-            for r in results:
-                try:
-                    info = r.payload.decode("utf-8")
-                except Exception:
-                    info = str(r.payload)
-                services.append(
-                    {
-                        "peer": str(r.key_expr),
-                        "info": info,
-                    }
+        all_connected = "DISCONNECT"
+        core_sw_list = []
+
+        for r in res:
+            if r["err"] is not None:
+                continue
+
+            core_sw_info = r["dict_payload"]
+            core_sw_info["connected"] = "DISCONNECT"
+
+            be_service = r["dict_payload"]["category"]
+            robot_namespace = r["dict_payload"]["name"]
+
+            if be_service == "manipulate":
+                topic, mv, obj, attachment = await zenoh_client.receive_one(
+                    f"{robot_namespace}/state_core",
+                    flatbuffer_obj_t=State_CoreT,
                 )
 
-            return services
-        except Exception as e:
-            raise HTTPException(status_code=502, detail=str(f"error: {e}")) from e
+                if obj["statusPowerOut"] == 1 and obj["statusServoNum"] == 1:
+                    all_connected = "CONNECTED"
+                    core_sw_info["connected"] = "CONNECTED"
+                elif obj["statusPowerOut"] == 1 or obj["statusServoNum"] == 1:
+                    all_connected = "UNSTABLE"
+                    core_sw_info["connected"] = "UNSTABLE"
+
+            elif be_service == "mobility":
+                topic, mv, obj, attachment = await zenoh_client.receive_one(
+                    f"{robot_namespace}/state",
+                )
+            elif be_service == "sensor":
+                topic, mv, obj, attachment = await zenoh_client.receive_one(
+                    f"{robot_namespace}/state_core",
+                )
+
+            core_sw_list.append(core_sw_info)
+
+        return {
+            "ip": ip,
+            "all_connected": all_connected,
+            "cpu_usage": psutil.cpu_percent(interval=1),
+            "core_sw_num": len(core_sw_list),
+            "core_sw_list": core_sw_list,
+        }
+
+    async def repeat_get_system_state(self):
+        next_ts = time.monotonic()
+
+        while True:
+            res = await self.get_system_state()
+
+            if self._prev_all_connected_status != res and socket_client.connected:
+                await socket_client.emit("system_state", res)
+                self._prev_all_connected_status = res
+
+            next_ts += self._all_connect_state_period
+            await asyncio.sleep(max(0, next_ts - time.monotonic()))
+
+    def get_all_robots_info(self):
+        req = Request_CallWhoAmIT()
+
+        results = zenoh_client.query_all(
+            "*/call_whoami",
+            flatbuffer_req_obj=req,
+            flatbuffer_res_T_class=Response_CallWhoamIT,
+            flatbuffer_buf_size=1024,
+        )
+
+        return results
 
     async def power_control(
         self, *, power_option: int, sync_servo: bool, stoptime: int | None = 0.5
@@ -46,7 +110,10 @@ class StateService:
             req.power_option = power_option
 
             res = zenoh_client.query_one(
-                "*/call_powercontrol", flatbuffer_obj=req, flatbuffer_buf_size=32
+                "*/call_powercontrol",
+                flatbuffer_req_obj=req,
+                flatbuffer_res_T_class=Response_FunctionsT,
+                flatbuffer_buf_size=32,
             )
 
             if sync_servo:
