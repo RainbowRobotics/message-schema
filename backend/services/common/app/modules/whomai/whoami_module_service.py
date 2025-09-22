6fe996c416475473715502df89e2f9b3769062fd
dfd1123 <dfd1123@naver.com>
2025-09-22 20:13:17 +0900

    fix: whoami 모듈 스키마 추가 및 비동기 호출 기능 개선

--- backend/services/common/app/modules/whomai/whoami_module_service.py
+++ backend/services/common/app/modules/whomai/whoami_module_service.py
@@ -1,24 +1,38 @@
-import flatbuffers
+import asyncio
+import time
+
+from app.socket import socket_client
 from flat_buffers.IPC.Request_CallWhoAmI import Request_CallWhoAmIT
 from flat_buffers.IPC.Response_CallWhoamI import Response_CallWhoamIT
 from rb_zenoh import zenoh_client
-from utils.parser import t_to_dict
 
 
 class WhoamiService:
     def __init__(self):
-        pass
+        self.period = 1
+        self.prev_whoami_res = []
 
     async def get_whoami(self):
         req = Request_CallWhoAmIT()
 
-        b = flatbuffers.Builder(32)
-        b.Finish(req.Pack(b))
-        fb_payload = bytes(b.Output())
+        res = zenoh_client.query_all(
+            "*/call_whoami",
+            flatbuffer_req_obj=req,
+            flatbuffer_res_T_class=Response_CallWhoamIT,
+            flatbuffer_buf_size=1024,
+        )
 
-        res = zenoh_client.query_one("*/call_whoami", payload=fb_payload)
+        return res
 
-        buf = res["payload"]
-        res = Response_CallWhoamIT.InitFromPackedBuf(buf, 0)
+    async def repeat_get_whoami(self):
+        next_ts = time.monotonic()
 
-        return t_to_dict(res)
+        while True:
+            res = await self.get_whoami()
+
+            if self.prev_whoami_res != res and socket_client.connected:
+                await socket_client.emit("whoami", res)
+                self.prev_whoami_res = res
+
+            next_ts += self.period
+            await asyncio.sleep(max(0, next_ts - time.monotonic()))
