a04d04432967735bd2aa01f7d0880e75c069484c
dfd1123 <dfd1123@naver.com>
2025-10-20 21:53:26 +0900

    feat: 추가된 소켓 라우터로 비동기 처리 기능 확장

--- backend/services/common/app/main.py
+++ backend/services/common/app/main.py
@@ -1,11 +1,13 @@
 from rb_modules.rb_fastapi_app import AppSettings, create_app
 
 from app.features.info.info_api import info_router
+from app.features.info.info_socket import info_socket_router
 from app.features.program.program_api import program_router
 from app.features.program.program_module import ProgramService
 from app.features.program.program_socket import program_socket_router
 from app.features.state.state_api import state_router
 from app.features.state.state_module import StateService
+from app.features.state.state_socket import state_socket_router
 from app.features.state.state_zenoh import zenoh_state_router
 from app.features.whoami.whoami_api import whoami_router
 from app.features.whoami.whoami_socket import whoami_socket_router
@@ -24,7 +26,12 @@ app = create_app(
     settings=setting,
     socket_client=socket_client,
     zenoh_routers=[zenoh_state_router],
-    socket_routers=[whoami_socket_router, program_socket_router],
+    socket_routers=[
+        whoami_socket_router,
+        program_socket_router,
+        state_socket_router,
+        info_socket_router,
+    ],
     api_routers=[state_router, whoami_router, info_router, program_router],
     bg_tasks=[
         state_service.repeat_get_system_state,
