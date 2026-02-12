ed52be186baf462ab38bf69751a0c33edc22e5c5
yujinHeo <yjheo4@rainbow-robotics.com>
2026-02-12 15:57:15 +0900

    fix: whoami_socket

--- backend/services/common/app/features/whoami/whoami_socket.py
+++ backend/services/common/app/features/whoami/whoami_socket.py
@@ -23,14 +23,13 @@ _prev_res: list[dict[str, Any]] = []
 
 @whoami_socket_router.on("{robot_model}/whoami")
 async def on_whoami(data, robot_model: str):
-    print("?????", robot_model, data)
-    # next_ts = time.monotonic()
+    next_ts = time.monotonic()
 
-    # while True:
-    #     res = await whoamiService.get_whoami(robot_model)
+    while True:
+        res = await whoamiService.get_whoami(robot_model)
 
-    #     if _prev_res != res:
-    #         await socket_client.emit("whoami", res)
+        if _prev_res != res:
+            await socket_client.emit("whoami", res)
 
-    #     next_ts += _period
-    #     await asyncio.sleep(max(0, next_ts - time.monotonic()))
+        next_ts += _period
+        await asyncio.sleep(max(0, next_ts - time.monotonic()))
