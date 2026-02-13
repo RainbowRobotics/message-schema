2aecd75b9e30c1352b6624c44c0232a13e3e0111
yujinHeo <yjheo4@rainbow-robotics.com>
2026-02-13 10:47:22 +0900

    feat: status

--- backend/services/amr/app/features/map/map_zenoh.py
+++ backend/services/amr/app/features/map/map_zenoh.py
@@ -5,8 +5,15 @@ from app.socket.socket_client import socket_client
 map_zenoh_router = ZenohRouter()
 
 @map_zenoh_router.subscribe(
-    "amr/{robot_model}/{robot_id}/socket/mappingCloud",
+    "amr/*/*/socket/mappingCloud",
     flatbuffer_obj_t=MappingCloudT
 )
-async def on_sub_slamnav_mappingCloud(robot_model: str, robot_id: str, *, topic, obj):
+async def on_sub_slamnav_mappingCloud(*, topic, obj):
+    # 로봇 모델과 아이디 추출
+    robot_model, robot_id = topic.split("/")[1:3]
+    # 로봇 모델과 아이디가 없으면 반환
+    if not robot_model or not robot_id:
+        return
+
+    # 소켓 클라이언트로 전송
     await socket_client.emit(topic, obj)
--- backend/services/amr/app/features/status/status_zenoh.py
+++ backend/services/amr/app/features/status/status_zenoh.py
@@ -9,26 +9,60 @@ from app.socket.socket_client import socket_client
 status_zenoh_router = ZenohRouter()
 
 @status_zenoh_router.subscribe(
-    "amr/{robot_model}/{robot_id}/status",
+    "amr/*/*/status",
     flatbuffer_obj_t=StatusT,
-    opts=SubscribeOptions(dispatch="queue", overflow="latest_only")
+    opts=SubscribeOptions(dispatch="queue", maxsize=2, overflow="latest_only")
 )
-async def on_sub_slamnav_status(robot_model: str, robot_id: str, *, topic, obj):
-    print("Status Sub : ", robot_model, robot_id, topic, obj, flush=True)
+async def on_sub_slamnav_status(*, topic, obj):
+    # 로봇 모델과 아이디 추출
+    robot_model, robot_id = topic.split("/")[1:3]
+    # 로봇 모델과 아이디가 없으면 반환
+    if not robot_model or not robot_id:
+        return
+    # print("Status Sub : ", robot_model, robot_id, topic, obj, flush=True)
+    # 소켓 클라이언트로 전송
     await socket_client.emit(topic, obj)
 
+    # common으로 전송
+    # await status_zenoh_router.publish(f"?/status", obj)
+
+
 @status_zenoh_router.subscribe(
-    "amr/{robot_model}/{robot_id}/moveStatus",
+    "amr/*/*/moveStatus",
     flatbuffer_obj_t=MoveStatusT,
     opts=SubscribeOptions(dispatch="queue", overflow="latest_only")
 )
-async def on_sub_slamnav_moveStatus(robot_model: str, robot_id: str, *, topic, obj):
+async def on_sub_slamnav_moveStatus(*, topic, obj):
+    # 로봇 모델과 아이디 추출
+    robot_model, robot_id = topic.split("/")[1:3]
+
+    # 로봇 모델과 아이디가 없으면 반환
+    if not robot_model or not robot_id:
+        return
+
+    # print("MoveStatus Sub : ", robot_model, robot_id, topic, obj, flush=True)
+
+    # 소켓 클라이언트로 전송
     await socket_client.emit(topic, obj)
 
+    # common으로 전송
+    # await status_zenoh_router.publish(f"?/moveStatus", obj)
+
 @status_zenoh_router.subscribe(
-    "amr/{robot_model}/{robot_id}/lidar2d",
+    "amr/*/*/socket/lidar2d",
     flatbuffer_obj_t=Lidar2DT,
     opts=SubscribeOptions(dispatch="queue", overflow="latest_only")
 )
-async def on_sub_slamnav_lidar2d(robot_model: str, robot_id: str, *, topic, obj):
+async def on_sub_slamnav_lidar2d(*, topic, obj):
+    # 로봇 모델과 아이디 추출
+    robot_model, robot_id = topic.split("/")[1:3]
+
+    # 로봇 모델과 아이디가 없으면 반환
+    if not robot_model or not robot_id:
+        return
+
+    # 소켓 클라이언트로 전송
     await socket_client.emit(topic, obj)
+
+    # common으로 전송
+    # await status_zenoh_router.publish(f"?/lidar2d", obj)
